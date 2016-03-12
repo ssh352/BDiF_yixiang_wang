3406786fece828be1cc54562e0aec9945b390b2c
wyx-cornell <yixiang.wang.ny@gmail.com>
2016-03-12 12:22:48 -0500

    hwb

--- assignment_B/transaction_generator.py
+++ assignment_B/transaction_generator.py
@@ -9,6 +9,9 @@ import datetime
 from random import randint
 from datetime import timedelta
 import csv
+from yahoo_finance import Share
+#in order to use yahoo_finance, you need to install it first
+#pip install yahoo-finance
 
 st= datetime.datetime(2016, 3, 7, 9, 45, 0, 00001)#set the default starting time
 print st + timedelta(microseconds=randint(0, 100000))
@@ -20,12 +23,36 @@ for i in range(N_rec):
     st+=timedelta(microseconds=randint(0, 100000))
     print st
     
+#reading the symbol data from csv file    
+f = open('DJ_data.csv', 'rb')
+reader = csv.reader(f)
+headers_dj = reader.next()
+
+symbol = {}
+for h in headers_dj:
+    symbol[h] = []
+    
+for row in reader:
+    for h, v in zip(headers_dj, row):
+        symbol[h].append(v)
+
+f.close()
+
+#print randint(0,29)
+#print random.uniform(1.5, 1.9)
+prices= range(30)
+for i in prices:
+    prices[i]=Share(symbol['Ticker'][i]).get_price()
+
+print prices
+
 with open ('data.txt', 'w') as fo:
    for i in range(N_rec):
        st+=timedelta(microseconds=randint(0, 100000))
-       fo.write(st.strftime('%m/%d/%Y/%H') +','+'good'+ '\n')
+       fo.write(st.strftime('%m/%d/%Y:%H:%M:%S.%f') +','+'good'+ '\n')
     
-    
+
+
 
 
 
