662fe607b27fa4e5222c2da1bf2ab37cc9ca85ef
wyx-cornell <yixiang.wang.ny@gmail.com>
2016-03-19 19:40:49 -0400

    hw3

--- assignment_C/code.py
+++ assignment_C/code.py
@@ -5,11 +5,45 @@ Created on Sat Mar 19 17:04:27 2016
 @author: wyx
 """
 import re
+import numpy as np
+import csv
+
+f = open('/Users/wyx/Documents/Baruch MFE/BDiF_yixiang_wang/assignment_C/LoughranMcDonald_MasterDictionary_2014.csv', 'rb')
+reader = csv.reader(f)
+headers = reader.next()
+
+wordlist = {}
+for h in headers:
+    wordlist[h] = []
+    
+for row in reader:
+    for h, v in zip(headers, row):
+        wordlist[h].append(v)
+
+f.close()
+
+print headers
+
+word_list={}
+
+for i in range(len(wordlist['Positive'])):
+    if wordlist['Positive'][i]=='2009':
+        temp=1
+    elif wordlist['Negative'][i]=='2009':
+        temp=-1
+    else:
+        temp=0
+    word_list[wordlist['Word'][i]]=temp
+    
+word_list['GOOD']
+    
+
+
 
 string_test='test:.[]da..gS***NOT////not..StuPid@@@@'
 
 stck_list={"JPM":1, "MS":1,"GS":1}
-word_list={"GOOD":1,"YES":1,"BAD":-1,"STUPID":-1,"NOT":-1}
+
 
 
 
